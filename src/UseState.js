b2a983e42675fe7b0b3a34a638b607530b2c588f
Miguel Hernández <miguelhernandezk@gmail.com>
2022-06-16 01:07:11 -0500

    3. UseEffect in UseState

--- src/UseState.js
+++ src/UseState.js
@@ -2,6 +2,23 @@ import React from "react";
 
 function UseState({ name }){
     const [error, setError] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
+
+    React.useEffect(() => {
+        console.log("Starting effect");
+        if(!!loading) {// React detecs as a change the first render, thus setting loading to true        
+            setTimeout(() => {
+                console.log("Validating code");
+
+                setLoading(false);
+
+                console.log("Completed");
+            }, 3000)
+        };
+        console.log("Ending effect");
+    }, [loading]);
+
+
     return(
         <div>
             <h2>Delete { name }</h2>
@@ -11,9 +28,15 @@ function UseState({ name }){
                 <p>Error: Incorrect code</p>
             )}
 
+            {loading && (
+                <p>Loading...</p>
+            )}
+
+
             <input placeholder="Security code"/>
             <button
-                onClick={() => setError(!error)}
+                onClick={() => setLoading(true)}
+
             >Let's go!</button>
         </div>
     );
