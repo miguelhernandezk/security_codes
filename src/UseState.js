5212330e8d4deb514d0a49e5f4d594c3bcbbb55e
Miguel Hernández <miguelhernandezk@gmail.com>
2022-06-17 01:39:43 -0500

    6. Working in useState as in classes

--- src/UseState.js
+++ src/UseState.js
@@ -3,25 +3,38 @@ import React from "react";
 const SECURITY_CODE = "paradigma";
 
 function UseState({ name }){
+    const [state, setState] = React.useState({ // Working state as in classes
+        value: "",
+        error: false,
+        loading: false,
+    });
+
     const [value, setValue] = React.useState("");
     const [error, setError] = React.useState(false);
     const [loading, setLoading] = React.useState(false);
 
-    console.log(value);
+    console.log(state);
 
     React.useEffect(() => {
         console.log("Starting effect");
-        if(!!loading) {// React detecs as a change the first render, thus setting loading to true        
+        if(!!state.loading) {// React detecs as a change the first render, thus setting loading to true        
             setTimeout(() => {
                 console.log("Validating code");
 
 
-                if(value === SECURITY_CODE){
-                    setLoading(false);
-                    setError(false); // Should be combined with A
+                if(state.value === SECURITY_CODE){
+                    setState({ // Work as in classes (NOT independent states)
+                        ...state,
+                        loading: false,
+                        error: false,
+                    });
+                    // setError(false); // Should be combined with A
                 }else{
-                    setError(true);
-                    setLoading(false);
+                    setState({
+                        ...state,
+                        loading: false,
+                        error: true,
+                    });
                 }
                 
 
@@ -29,7 +42,7 @@ function UseState({ name }){
             }, 3000)
         };
         console.log("Ending effect");
-    }, [loading]);
+    }, [state.loading]);
 
 
     return(
@@ -37,28 +50,32 @@ function UseState({ name }){
             <h2>Delete { name }</h2>
             <p>Please, write the secutiry code:</p>
 
-            {(error && !loading) && (  // A:  This works without updating the state
+            {(state.error && !state.loading) && (  // A:  This works without updating the state
                 <p>Error: Incorrect code</p>
             )}
 
-            {loading && (
+            {state.loading && (
                 <p>Loading...</p>
             )}
 
 
             <input 
-                value = {value}
+                value = {state.value}
                 placeholder="Security code"
                 onChange={(event) =>{
-                    // setError(false); // It can be updated here bt it will disappear when users enters something into the input.
-                    setValue(event.target.value);
+                    setState({
+                        ...state,
+                        value: event.target.value,
+                    });
                 }}
             />
             <button
                 onClick={() => 
                     {
-                        setLoading(true);
-                        // setError(false); // This works fine
+                        setState({
+                            ...state,
+                            loading: true,
+                        });
                     }
                 }
             >Let's go!</button>
